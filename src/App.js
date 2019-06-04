d24ce2583ba2fbee362bd84afc7ad3af04e8f697
Denny Luan <dennyluan@gmail.com>
2019-06-03 18:51:08 -0700

    Adds react-router, an about page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
 import React from 'react';
-import './App.scss';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
-function App() {
+import './App.scss';
+import About from './About';
+
+const Index = ({}) => {
   return (
-    <div className="App">
-      <div id="home">
-        <div className="container center">
-          <div className="row">
-            <div className="col-xs-12 col-md-12">
-              <div id="pokemans">
-                <div id="pikachu"></div>
-                <div id="ash"></div>
-              </div>
-              <p>Denny Luan</p>
+    <div id="home">
+      <div className="container center">
+        <div className="row">
+          <div className="col-xs-12 col-md-12">
+            <div id="pokemans">
+              <div id="pikachu"></div>
+              <div id="ash"></div>
             </div>
+            <h1>Hello!</h1>
+            <p>I'm Denny Luan, welcome to my web page.</p>
           </div>
-          <div className="row">
-            <div className="col-md-12">
-              <ul>
-                <li>
-                  <a href="https://twitter.com/dennyluan">Twitter</a>
-                </li>
-                <li>
-                  <a href="https://experiment.com">Experiment</a>
-                </li>
-                <li>
-                  <a href="https://research.pizza">Research Pizza</a>
-                </li>
-              </ul>
-            </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <ul>
+              <li>
+                <Link to={'chronology'}>About Me</Link>
+              </li>
+
+              <li>
+                <a href="https://twitter.com/dennyluan">Twitter</a>
+              </li>
+
+              <li>
+                <a href="https://experiment.com">Experiment</a>
+              </li>
+
+              <li>
+                <a href="https://research.pizza">Research Pizza</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
+  )
+}
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        
+      </div>
+
+      <Route path="/" exact component={Index}></Route>
+      <Route path="/chronology/" component={About}></Route>
+    </Router>
   );
 }
 
